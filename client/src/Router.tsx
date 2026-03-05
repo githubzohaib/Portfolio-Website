b30ebb986f6fd94602de6e201bb146835067594f
Zohaib Ali <zohaibaliwork@gmail.com>
2026-03-05 23:57:51 +0500

    Router updated

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,21 +1,61 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
-import Applayout from "./components/layouts/AppLayout";
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "./components/layouts/AppLayout";
 import NoMatch from "./pages/NoMatch";
 import Error500 from "./pages/Error500";
+import Hero from "./components/layouts/Hero";
+import AboutMe from "./components/layouts/AboutMe";
+import Skills from "./components/layouts/Skills";
+import Projects from "./components/layouts/Projects";
+import Internship from "./components/layouts/Internship";
+import HackExp from "./components/layouts/HackExp";
+import Contact from "./components/layouts/Contact";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Hero />,
+        },
+        {
+          path: "/about",
+          element: <AboutMe />,
+        },
+        {
+          path: "/skills",
+          element: <Skills />,
+        },
+        {
+          path: "/projects",
+          element: <Projects />,
+        },
+        {
+          path: "/internship",
+          element: <Internship />,
+        },
+        {
+          path: "/hackexp",
+          element: <HackExp />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+    {
+      path: "/error",
+      element: <Error500 />,
+    },
+    {
+      path: "*",
+      element: <NoMatch />,
+    },
+  ],
   {
-    path: "/",
-    element: <Applayout/>,
-  },
-  {
-    path: "/error", // ✅ NEW
-    element: <Error500 />,
-  },
-  {
-    path: "*",
-    element: <NoMatch />,
-  },
-], {
-  basename: global.basename
-});
+    basename: global.basename,
+  }
+);
